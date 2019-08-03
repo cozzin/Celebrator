dadc0f2d67fe5d59510a63d2e7f7a390e3f3e9e4
seongho.hong <hsh3592@gmail.com>
2019-08-03 22:58:27 +0900

    Initial Commit

---
+++ Celebrator/Celebrator.h
@@ -0,0 +1,19 @@
+//
+//  Celebrator.h
+//  Celebrator
+//
+//  Created by 홍성호 on 03/08/2019.
+//  Copyright © 2019 cozzin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Celebrator.
+FOUNDATION_EXPORT double CelebratorVersionNumber;
+
+//! Project version string for Celebrator.
+FOUNDATION_EXPORT const unsigned char CelebratorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Celebrator/PublicHeader.h>
+
+
